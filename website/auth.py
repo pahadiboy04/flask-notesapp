a250f8351add88421812701bacbb8ea721119264
Anurag gusain <123312935+pahadiboy04@users.noreply.github.com>
2025-03-23 22:06:19 +0530

    Add files via upload

---
+++ website/auth.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask import flash
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
+from . import db
+from flask_login import login_user,login_required,logout_user,current_user
+
+auth=Blueprint('auth',__name__)
+@auth.route('/login', methods=['GET','POST'])
+def login():
+    if request.method=="POST":
+        email=request.form.get('email')
+        password=request.form.get('password')
+        user=User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully!',category='success')
+                login_user(user,remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again',category='error')
+        else:
+            flash('Email does not exist',category='error')
+    
+    return render_template("index.html",user=current_user)
+@auth.route('/logout')
+@login_required
+def logout():
+   logout_user()
+   return redirect(url_for('auth.login'))
+@auth.route('/sign-up', methods=['GET','POST'])
+def sign_up():
+    if request.method=='POST':
+        email=request.form.get('email')
+        username=request.form.get('username')
+        password=request.form.get('password1')
+        confirmPassword=request.form.get('password2')
+        print(email,username,password,confirmPassword)
+        if User.query.filter_by(username=username).first():
+            flash('Username already exists', category='error')
+        elif  User.query.filter_by(email=email).first():
+            flash('Email already exists', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        elif confirmPassword != password:
+            flash('Passwords don\'t match', category='error')
+        else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user,remember=True)
+            flash('Account created', category='success')
+            return redirect(url_for('views.home'))
+        
+    return render_template("sign-up.html",user=current_user)
+
+
